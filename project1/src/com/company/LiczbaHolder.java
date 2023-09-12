1c064cfcef582e99efac45ae07324318d4c2fff2
michal.domin06 <michal.domin06@gmail.com>
2023-09-12 13:21:01 +0200

    liczba holder class creating

--- project1/src/com/company/LiczbaHolder.java
+++ project1/src/com/company/LiczbaHolder.java
@@ -1,2 +1,17 @@
-package com.company;public class LiczbaHolder {
+package com.company;
+
+public class LiczbaHolder {
+    int a = 5;
+
+    public LiczbaHolder(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 }
