4772e3133655e52706bd9304650fa1acf4bae6a0
michal.domin06 <michal.domin06@gmail.com>
2023-09-12 13:20:22 +0200

    creating two objects LiczbaHolder in Main

--- project1/src/com/company/Main.java
+++ project1/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	LiczbaHolder lh1 = new LiczbaHolder(12);
+    LiczbaHolder lh2 = new LiczbaHolder(44);
     }
 }
